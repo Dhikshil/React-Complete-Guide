519b8788ce0263fcec37746aa2eec2871c46ce4f
Dhikshil <mdhikshil@gmail.com>
2025-07-20 18:35:48 +0100

    completed and organised dynamic code to App.jsx

--- Section 3 - React Essentials/01-starting-project/src/App.jsx
+++ Section 3 - React Essentials/01-starting-project/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES } from './data.js';
 import { Header } from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState(false);
 
   function handleClick(selectedButton) {
      setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please Select a topic</p>
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -31,8 +45,8 @@ function App() {
             <TabButton onClick = {() => handleClick('jsx')}>JSX</TabButton>
             <TabButton onClick = {() => handleClick('props')}>Props</TabButton>
             <TabButton onClick = {() => handleClick('state')}>State</TabButton>
-          </menu>
-          {selectedTopic}
+          </menu> 
+              {tabContent}
         </section>
       </main>
     </div>  
