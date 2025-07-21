5c9e7f56633ef8ef5817780f724d6285ab1a3dde
Dhikshil <mdhikshil@gmail.com>
2025-07-21 13:12:41 +0100

    fixed some errors

--- Section 4 - React Essentials Deep Dive/01-starting-project/src/components/Examples.jsx
+++ Section 4 - React Essentials Deep Dive/01-starting-project/src/components/Examples.jsx
@@ -31,7 +31,6 @@ export default function Examples() {
         <>
             <Section title="Examples" id="examples">
                 <Tabs 
-                buttonsContainer = "menu"
                 buttons={
                     <>
                         <TabButton
