14e692cfd073c4c16bc2e2e8f80ea841f844a4db
Dhikshil <mdhikshil@gmail.com>
2025-07-24 15:01:49 +0100

    added new player log functionality that works with the Log component as well as the GameBoard component

--- Section 4 - React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section 4 - React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react"
 import Player from "./components/Player.jsx"
 import GameBoard from "./components/GameBoard.jsx"
+import Log from "./components/Log.jsx";
+
 
 function App() {
+  const [ gameTurns, SetGameTurns ] = useState([]);
   const [ activePlayer, setActivePlayer ] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currentActivePlayer) => currentActivePlayer === "X" ? "O" : "X");
+    SetGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O"
+      }
+
+      const updatedTurns = [{ square: {rowIndex, col: colIndex }, player: currentPlayer}, ...prevTurns];
+
+      return updatedTurns;
+    })
   }
 
   return (<main>
@@ -17,6 +30,7 @@ function App() {
       </ol>
       <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
     </div>
+    
   </main>
   )
 }
