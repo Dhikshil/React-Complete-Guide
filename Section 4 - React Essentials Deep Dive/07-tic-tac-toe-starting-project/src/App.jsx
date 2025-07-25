097f2cf09e3cefe6219cd07793d992a6e6b23ca6
Dhikshil <mdhikshil@gmail.com>
2025-07-25 11:11:37 +0100

    tic-tac-toe project hard reset

--- Section 4 - React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section 4 - React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,39 +1,10 @@
-import { useState } from "react"
-import Player from "./components/Player.jsx"
-import GameBoard from "./components/GameBoard.jsx"
-import Log from "./components/Log.jsx";
-
 
 function App() {
-  const [ gameTurns, SetGameTurns ] = useState([]);
-  const [ activePlayer, setActivePlayer ] = useState("X");
+  
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === "X" ? "O" : "X");
-    SetGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O"
-      }
-
-      const updatedTurns = [{ square: {rowIndex, col: colIndex }, player: currentPlayer}, ...prevTurns];
-
-      return updatedTurns;
-    })
-  }
-
-  return (<main>
-    <div id="game-container" className="highlight-player">
-      <ol id="players">
-        <Player initialName = "Player 1" symbol = "X" isActive={activePlayer === "X"}/>
-        <Player initialName = "Player 2" symbol = "O" isActive={activePlayer === "O"}/>
-      </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
-    </div>
-    
-  </main>
+  return (
+    <h1>React Tic-Tac-Toe</h1>
   )
 }
 
 export default App
- 
