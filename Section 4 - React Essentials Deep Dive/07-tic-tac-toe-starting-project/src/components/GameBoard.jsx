7a8c67e64f1215c4bf6033ea2b9fd11c41f67cca
Dhikshil <mdhikshil@gmail.com>
2025-07-25 12:50:19 +0100

    updated game-board to allow editing the array values

--- Section 4 - React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ Section 4 - React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,10 +7,24 @@ const initialGameBoard = [
 ]
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard ] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const currentGameBoard = [...prevGameBoard.map(innerArr => [...innerArr])];
+            currentGameBoard[rowIndex][colIndex] = "X";
+            return currentGameBoard;
+            
+        });
+    }
+
     return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
-                {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
+                {row.map((playerSymbol, colIndex) =>
+                    <li key={colIndex}>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                    </li>)}
             </ol>
         </li>)}
     </ol>
