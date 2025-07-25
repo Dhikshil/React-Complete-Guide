b406ed9f1f58332d61fba3ae5c2a109123a76160
Dhikshil <mdhikshil@gmail.com>
2025-07-25 17:11:26 +0100

    added draw functionality

--- Section 4 - React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section 4 - React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,6 +4,7 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+import GameOver from "./components/GameOver.jsx";
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -48,6 +49,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) =>{
       const currentPlayer = deriveActivePlayer(prevTurns);
@@ -68,7 +71,7 @@ function App() {
           <Player initalName="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initalName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board = {gameBoard} />
       </div>
       <Log turns = {gameTurns} />
