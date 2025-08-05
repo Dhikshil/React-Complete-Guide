eefeab442d6264a77a73bc712d719f0aa89f5059
Dhikshil <mdhikshil@gmail.com>
2025-08-05 16:23:54 +0100

    added clunky unoptimised code which takes too long for limited functionality

--- Section 8 - Refs and Portals/01-starting-project/src/components/Player.jsx
+++ Section 8 - Refs and Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  
+  function handleNameChange(event) {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handleButton() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={handleNameChange} value={playerName} type="text" />
+        <button onClick={handleButton}>Set Name</button>
       </p>
     </section>
   );
