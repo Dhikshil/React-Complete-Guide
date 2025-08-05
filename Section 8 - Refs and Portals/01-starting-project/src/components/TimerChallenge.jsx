1184500eac07ce37d91619fc7922219f8906162c
Dhikshil <mdhikshil@gmail.com>
2025-08-05 17:15:33 +0100

    added timer challenges, able to start timer

---
+++ Section 8 - Refs and Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    function handleStart() {
+        setTimerStarted(true);
+        setTimeout(() => {setTimerExpired(true);}, targetTime * 1000);
+    }
+
+    function handleStart() {
+        
+    }
+
+    return <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You Lost!</p>}
+        <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
+        <p>
+            <button onClick={handleStart}>{timerStarted ? "Stop Timer" : "Start Challenge"}</button>
+        </p>
+        <p className={timerStarted ? "actice" : undefined}>{timerStarted ? "Timer is running" : "Timer shut down"}</p>
+    </section>
+}
