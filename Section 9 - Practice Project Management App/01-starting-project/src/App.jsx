a4dc39cd014a45cf046b5aeb5cee1cc47d86a456
Dhikshil <mdhikshil@gmail.com>
2025-08-07 14:19:27 +0100

    able to go from undefined to add project page

--- Section 9 - Practice Project Management App/01-starting-project/src/App.jsx
+++ Section 9 - Practice Project Management App/01-starting-project/src/App.jsx
@@ -2,13 +2,35 @@ import ProjectSidebar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
-
+import { useState } from "react";
 
 function App() {
+  const [ projectsState, setProjectsState ] = useState({
+    selectedProject: undefined,
+    projects: [],
+  }); 
+
+  function handlestartAddProject() {
+    setProjectsState((prevProjectsState) => {
+      return {
+        ...prevProjectsState,
+        selectedProject: null,
+      }
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProject === null) {
+    content = <NewProject />
+  } else {
+    content = <NoProjectSelected onStartAddProject={handlestartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handlestartAddProject} />
+      {content}
     </main>
   );
 }
