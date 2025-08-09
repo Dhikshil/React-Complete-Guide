8db40154180457369e5ed6713c59a24bcdc34881
Dhikshil <mdhikshil@gmail.com>
2025-08-09 18:05:21 +0100

    added more function

--- Section 9 - Practice Project Management App/01-starting-project/src/App.jsx
+++ Section 9 - Practice Project Management App/01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import ProjectSidebar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
+import SelectedProject from "./components/SelectedProject";
 
 import { useState } from "react";
 
@@ -43,17 +44,28 @@ function App() {
     });
   }
 
-  let content;
+  function handleSelectProject(id) {
+      setProjectsState((prevProjectsState) => {
+      return {
+        ...prevProjectsState,
+        selectedProjectId: id,
+      }
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(project => project.id === project.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} />
 
   if (projectsState.selectedProject === null) {
     content = <NewProject onSave={handleAddProject} onCancel={handleCancelProject} />
-  } else {
+  } else if (projectsState.selectedProject === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   };
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} onSelectProject={handleSelectProject} />
       {content}
     </main>
   );
