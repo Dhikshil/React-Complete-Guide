cad1a2004e4a5c3a19cd19206f6af622c2e8adc5
Dhikshil <mdhikshil@gmail.com>
2025-08-12 20:43:13 +0100

    able to retrieve data

--- Section 15 - HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ Section 15 - HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react';
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvaialablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((response) => {
+        return response.json()
+      })
+      .then((resData) => {
+        setAvaialablePlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
