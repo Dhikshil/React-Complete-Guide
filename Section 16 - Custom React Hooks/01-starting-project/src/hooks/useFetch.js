33f3eecb2a7c2b3ce77656b48c4c936cd9a855e5
Dhikshil <mdhikshil@gmail.com>
2025-08-13 16:18:45 +0100

    fully used custom fetch hook

--- Section 16 - Custom React Hooks/01-starting-project/src/hooks/useFetch.js
+++ Section 16 - Custom React Hooks/01-starting-project/src/hooks/useFetch.js
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import { fetchUserPlaces } from "../http.js";
-
 
 export function useFetch( fetchFn, initialValue ) {
     const [isFetching, setIsFetching] = useState();
