ed7eeb7289dede14ebcec83297b2ba15fcc8fc0a
Dhikshil <mdhikshil@gmail.com>
2025-08-13 09:51:07 +0100

    added code to handle any bad request errors and show an error banner

--- Section 15 - HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ Section 15 - HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,22 +1,42 @@
 import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
+import ErrorPage from './Error.jsx'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvaialablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvaialablePlaces(resData.places);
+    
+      try {
+        const response = await fetch('http://localhost:3000/placess');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          const error = new Error('Failed to fetch places')
+          throw error
+        }
+        
+        setAvaialablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message: error.message ||  'Could not fetch places.'
+        })
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+  
+  if (error) {
+    return <ErrorPage title='Error occurred!' message={error.message} />
+  }
 
   return (
     <Places
