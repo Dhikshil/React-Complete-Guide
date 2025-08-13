2f5ce0061340f069f2a7a2ac70fe731a5aa8fe1c
Dhikshil <mdhikshil@gmail.com>
2025-08-13 11:44:23 +0100

    can delete data in backend

--- Section 15 - HTTP Requests/01-starting-project/src/App.jsx
+++ Section 15 - HTTP Requests/01-starting-project/src/App.jsx
@@ -49,8 +49,17 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
+    try {
+      await updateUserPlaces(
+        userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+      );
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setUpdatingPlacesError({message: error.message || 'Failed to delete place.'})
+    }
+
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
 
   function handleError() {
     setUpdatingPlacesError(null);
