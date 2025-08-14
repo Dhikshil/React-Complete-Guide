ffe91945ad43614d6eaf9761ef481c8f1baf3f55
Dhikshil <mdhikshil@gmail.com>
2025-08-14 17:22:14 +0100

    added optimistic votes

--- Section 18 - Form Actions/06-adv-starting-project/src/components/Opinion.jsx
+++ Section 18 - Form Actions/06-adv-starting-project/src/components/Opinion.jsx
@@ -1,16 +1,21 @@
-import { use, useActionState } from "react";
+import { use, useActionState, useOptimistic } from "react";
 
 import { OpinionsContext } from "../store/opinions-context";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(votes, (prevVotes, mode) => mode === 'up' ? prevVotes + 1 : prevVotes - 1);
+
   async function upvoteAction() {
+    setVotesOptimistically('up');
     await upvoteOpinion(id);
-  }
+  };
 
   async function downvoteAction() {
+    setVotesOptimistically('down');
     await downvoteOpinion(id);
-  }
+  };
   
   const [upvoteFormState, upvoteFormAction, upvotePending] = useActionState(upvoteAction);
   const [downvoteFormState, dwonvoteFormAction, dwonvotePending] = useActionState(downvoteAction);
@@ -41,7 +46,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
         <button formAction={dwonvoteFormAction} disabled={upvotePending || dwonvotePending}>
           <svg
