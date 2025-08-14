4daadcc2c9aa04241c30f60edc935d49060618a8
Dhikshil <mdhikshil@gmail.com>
2025-08-14 09:36:27 +0100

    built in validation

--- Section 17 - Forms and User Input/01-starting-project/src/components/Login.jsx
+++ Section 17 - Forms and User Input/01-starting-project/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 export default function Login() {  
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef(); 
 
@@ -9,6 +11,16 @@ export default function Login() {
     
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
   };
 
   return (
@@ -19,6 +31,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -28,7 +43,7 @@ export default function Login() {
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        <button type = "reset" className="button button-flat">Reset</button>
         <button className="button">Login</button>
       </p>
     </form>
